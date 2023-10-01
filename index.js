d1b83483e5550bd69bfe089b2530d77293e11e30
sarajouma <144836947+sarajouma@users.noreply.github.com>
2023-10-01 19:42:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let multiply = 0;
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
+/*-------------------------*/
+let random = Math.floor(Math.random() * 0) + 1;
+/*------------------------*/
+let mod = 0;
+let num3 = 10;
+let num4 = 6;
+mod = num3%num4;
+/*----------------------*/
+let max =Math.max(0,20);
